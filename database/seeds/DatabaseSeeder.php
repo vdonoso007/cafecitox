66d04cadf718452f656a6ef5eb0c846d42038541
vdonoso007 <vfda2710@gmail.com>
2019-08-20 02:14:34 -0500

    creacion grupo de productos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserTableSeeder::class);
-        //$this->call(ProductGroupTableSeeder::class);
+        //$this->call(UserTableSeeder::class);
+        $this->call(ProductGroupTableSeeder::class);
         //$this->call(ProductTableSeeder::class);
     }
 }
